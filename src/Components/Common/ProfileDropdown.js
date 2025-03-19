d757cc29c7cfdb0e4981ef92b001c2ab480961e7
shrek0430 <206593158+shrek0430@users.noreply.github.com>
2025-03-19 18:03:27 +0530

    changeFiles

--- src/Components/Common/ProfileDropdown.js
+++ src/Components/Common/ProfileDropdown.js
@@ -1,65 +1,73 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { createSelector } from "reselect";
-import { useSelector } from "react-redux";
-import avatar1 from "../../assets/images/users/avatar-2.jpg";
-import BaseDropdown from "../Base/Dropdown";
-import { DropDown } from "../../Components/constants/LoginConstant";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
+import { createSelector } from 'reselect';
+import { useSelector } from 'react-redux';
+import { DropDown } from '../constants/LoginConstant';
 
+import avatar1 from "../../assets/images/users/avatar-1.jpg";
 
 const ProfileDropdown = () => {
+
+
     const profiledropdownData = createSelector(
         (state) => state.Profile,
-        (state) => ({ user: state.user })
+        (state) => ({
+            user: state.user
+        })
     );
 
-    const { user } = useSelector(profiledropdownData);
+    const {user} = useSelector(profiledropdownData);
+
     const [userName, setUserName] = useState("Admin");
 
     useEffect(() => {
         if (sessionStorage.getItem("authUser")) {
             const obj = JSON.parse(sessionStorage.getItem("authUser"));
-            setUserName(
-                process.env.REACT_APP_DEFAULTAUTH === "fake"
-                    ? obj.username || user.first_name || "Admin"
-                    : process.env.REACT_APP_DEFAULTAUTH === "firebase"
-                    ? obj.email || "Admin"
-                    : "Admin"
+            setUserName(process.env.REACT_APP_DEFAULTAUTH === "fake" ? obj.username === undefined ? user.first_name ? user.first_name : obj.data.first_name : "Admin" || "Admin" :
+                process.env.REACT_APP_DEFAULTAUTH === "firebase" ? obj.email && obj.email : "Admin"
             );
         }
-    }, [user]);
+    }, [userName, user]);
 
-    const dropdownItems = [
-        <h6 className="dropdown-header">Welcome {userName}!</h6>,
-        <Link to="/profile" className="dropdown-item">
-            <i className="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i>
-            <span className="align-middle">{DropDown.Profile}</span>
-        </Link>,
-        <Link to="/change-password" className="dropdown-item">
-            <i className="mdi mdi-lock-reset text-muted fs-16 align-middle me-1"></i>
-            <span className="align-middle">{DropDown.ChangePassword}</span>
-        </Link>,
-        <div className="dropdown-divider"></div>,
-        <Link to="/logout" className="dropdown-item">
-            <i className="mdi mdi-logout text-muted fs-16 align-middle me-1"></i>
-            <span className="align-middle">{DropDown.Logout}</span>
-        </Link>,
-    ];
-
+    //Dropdown Toggle
+    const [isProfileDropdown, setIsProfileDropdown] = useState(false);
+    const toggleProfileDropdown = () => {
+        setIsProfileDropdown(!isProfileDropdown);
+    };
     return (
-        <BaseDropdown
-            toggleText={
-                <span className="d-flex align-items-center">
-                    <img className="rounded-circle header-profile-user" src={avatar1} alt="Avatar" />
-                    <span className="text-start ms-xl-2 d-none d-xl-inline-block fw-medium user-name-text">
-                        {userName}
+        <React.Fragment>
+            <Dropdown isOpen={isProfileDropdown} toggle={toggleProfileDropdown} className="ms-sm-3 header-item topbar-user">
+                <DropdownToggle tag="button" type="button" className="btn">
+                    <span className="d-flex align-items-center">
+                        <img className="rounded-circle header-profile-user" src={avatar1}
+                            alt="Header Avatar" />
+                        <span className="text-start ms-xl-2">
+                            <span className="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{userName}</span>
+                            <span className="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Founder</span>
+                        </span>
                     </span>
-                </span>
-            }
-            items={dropdownItems}
-            className="ms-sm-3 header-item topbar-user"
-            buttonClass="btn"
-        />
+                </DropdownToggle>
+                <DropdownMenu className="dropdown-menu-end">
+                    <h6 className="dropdown-header">Welcome {userName}!</h6>
+                    <DropdownItem className='p-0'>
+                        <Link to= "/profile" className="dropdown-item">
+                            <span className="align-middle">{DropDown.Profile}</span>
+                        </Link>
+                    </DropdownItem>
+                    <DropdownItem className='p-0'>
+                        <Link to= "/apps-chat" className="dropdown-item">
+                            <span className="align-middle">{DropDown.ChangePassword}</span>
+                        </Link>
+                    </DropdownItem>
+                    <DropdownItem className='p-0'>
+                        <Link to= "/logout" className="dropdown-item">
+                            <span className="align-middle" data-key="t-logout">Logout</span>
+                        </Link>
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+        </React.Fragment>
     );
 };
 
